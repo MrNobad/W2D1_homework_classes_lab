2be8f36c9fd2d13b4f2a0ffb55d826bfd52f40ae
Stephen Boyle <1nobad@gmail.com>
2019-09-16 22:05:40 +0100

    up to won lost section

--- specs/team_lab_spec.rb
+++ specs/team_lab_spec.rb
@@ -6,22 +6,36 @@ class TestSportTeam < MiniTest::Test
 
   def test_get_team_name
     team = SportTeam.new("CodeClan FC", ["Jack", "Bob", "Frank", "Farquar"], "Stevie Nobad")
-    assert_equal("CodeClan FC", team.get_team_name())
+    assert_equal("CodeClan FC", team.team_name())
   end
 
   def test_get_player_name
     team = SportTeam.new("CodeClan FC", ["Jack", "Bob", "Frank", "Farquar"], "Stevie Nobad")
-    assert_equal(["Jack", "Bob", "Frank", "Farquar"], team.get_player_name())
+    assert_equal(["Jack", "Bob", "Frank", "Farquar"], team.player_name())
   end
 
   def test_get_coach_name
     team = SportTeam.new("CodeClan FC", ["Jack", "Bob", "Frank", "Farquar"], "Stevie Nobad")
-    assert_equal("Stevie Nobad", team.get_coach_name())
+    assert_equal("Stevie Nobad", team.coach_name())
   end
 
   def test_set_coach_name
     team = SportTeam.new("CodeClan FC", ["Jack", "Bob", "Frank", "Farquar"], "Stevie Nobad")
-    team.set_coach_name "Gerry McGuire"
-    assert_equal("Gerry McGuire", team.get_coach_name())
+    team.coach_name = ("Gerry McGuire")
+    assert_equal("Gerry McGuire", team.coach_name())
   end
+
+  def test_add_player
+    team = SportTeam.new("CodeClan FC", ["Jack", "Bob", "Frank", "Farquar"], "Stevie Nobad")
+    team.add_player()
+    assert_equal(["Jack", "Bob", "Frank", "Farquar", "Ryan"], team.player_name())
+  end
+
+  def test_check_player_name
+    team = SportTeam.new("CodeClan FC", ["Jack", "Bob", "Frank", "Farquar"], "Stevie Nobad")
+    team.check_player_name()
+    assert_equal(false, team.check_player_name())
+  end
+
+  
 end
--- team_lab.rb
+++ team_lab.rb
@@ -1,12 +1,24 @@
 class SportTeam
 
-attr_accessor(:team_name, :player_name, :coach_name)
+attr_accessor(:team_name, :player_name, :coach_name, :points)
 
   def initialize(input_team_name, input_player_name, input_coach_name)
       @team_name = input_team_name
       @player_name = input_player_name
       @coach_name = input_coach_name
+      @points = 0
   end
+
+def add_player
+  @player_name << "Ryan"
+end
+
+def check_player_name
+  @player_name.include? "Ryan"
+  return false
+end
+
+
 
   # def get_team_name
   #   return @team_name
